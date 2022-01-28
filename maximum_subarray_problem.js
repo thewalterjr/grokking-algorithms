303954f7eaba3ec3e7a5984b2e15110dcbd8cce3
thewalterjr <89614640+thewalterjr@users.noreply.github.com>
2022-01-27 23:06:37 -0300

    Update maximum_subarray_problem.js

--- maximum_subarray_problem.js
+++ maximum_subarray_problem.js
@@ -10,6 +10,22 @@ function maximumSumSubarray(arr) {
   return maxSum;
 }
 
+function maxSumSubArray(arr) {
+  let localMax = arr[0];
+  let globalMax = arr[0];
+
+  for(let i = 1; i < arr.length; i++) {
+    localMax = Math.max(arr[i], arr[i] + localMax);
+    if (localMax > globalMax) {
+      globalMax = localMax;
+    }
+  }
+  return globalMax;
+}
+
 
 arr = [4, -1, 2, 1];
-console.log(maxSumSubArray(arr));
+console.log(maximumSumSubarray(arr)); // 6
+
+secondArr = [2, -5, 10, -1, 4, -10];
+console.log(maxSumSubArray(secondArr)); // 13
